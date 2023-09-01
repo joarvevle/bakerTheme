140a7c6edc4cacd3158b697d5b3613d661527618
joarvevle <143795683+joarvevle@users.noreply.github.com>
2023-09-02 00:31:16 +0200

    Update Baker_theme.R
    added dark theme minor adjustment to light theme

--- R/Baker_theme.R
+++ R/Baker_theme.R
@@ -1,16 +1,67 @@
 theme_baker <- function(){
-  theme_bw() %+replace%
+  theme_minimal() %+replace%
     theme(
-      axis.title = element_text(
-        color = "#018374",size=14,face="bold"),
-      axis.text = element_text(
-        color = '#05322b',size = 12,face="bold"),
-      plot.title = element_text(
-        color = '#018374',face="bold",size=22, hjust = 0 , vjust = 2),
-      legend.title = element_text(color = '#018374', size = 14),
+      #change font
+      #text=element_text(family="Poppins"), need to include poppins in package
+      
+      #title, axis title and legend title
+      plot.title = element_text(color = '#018374',face="bold",size=22, hjust = 0 , vjust = 2),    
+      axis.title = element_text(color = "#018374",size=14,face="bold"),    
+      legend.title = element_text(color = '#018374', size = 14, hjust = 0),    
+      #subtitle and caption and legend text
       legend.text = element_text(color = '#05322b',size = 14),
       plot.subtitle = element_text(color = "#05322b", hjust = 0, vjust = 2),
-      plot.caption = element_text(color = "#05322b", hjust = 1)
+      plot.caption = element_text(color = "#05322b", hjust = 1),
+      
+      #Name for category
+      axis.text.x = element_text(color = '#05322b',size = 12,face="bold"),
+      axis.text.y = element_text(color = '#05322b',size = 12,face="bold"),
+      axis.title.x = element_text(hjust = 0.5),
+      panel.grid.major.x = element_blank(),
+      
+      #Change grid lines to ligth grey
+      panel.grid.major.y = element_line(colour="#D0D0D0"),
+      panel.grid.minor.y = element_line(colour="#D0D0D0"),
+      
+      #legend.position = "bottom",    To move the legend to bottom. I dont like it
     )
 
  }
+
+theme_baker_dark <- function(){
+  theme_minimal() %+replace%
+    theme(
+      #change font
+      #text=element_text(family="Poppins"), need to include poppins in package
+      
+      #Change background to dark
+      panel.background = element_rect(fill = "#05322b"),
+      plot.background = element_rect(fill= "#05322b"), 
+      
+      #add ligth grey box around panel and legend
+      legend.background = element_rect(colour = "#D0D0D0", fill=NA),
+      panel.border = element_rect(colour = "#D0D0D0", fill=NA, size=.01),      
+      
+      #title, axis title and legend title
+      plot.title = element_text(color = '#FFFFFF',face="bold",size=22, hjust = 0 , vjust = 2),    
+      axis.title = element_text(color = "#FFFFFF",size=14,face="bold"),    
+      legend.title = element_text(color = '#FFFFFF', size = 14, hjust = 0),
+      #subtitle and caption and legend text
+      legend.text = element_text(color = '#D0D0D0',size = 14),
+      plot.subtitle = element_text(color = "#D0D0D0", hjust = 0, vjust = 2),
+      plot.caption = element_text(color = "#D0D0D0", hjust = 1),
+      
+      #Name for category
+      axis.text.x = element_text(color = '#FFFFFF',size = 12,face="bold"),
+      axis.text.y = element_text(color = '#FFFFFF',size = 12,face="bold"),
+      axis.title.x = element_text(hjust = 0.5),
+      panel.grid.major.x = element_blank(),
+      
+      #Change grid lines to ligth grey
+      panel.grid.major.y = element_line(colour="#D0D0D0"),
+      panel.grid.minor.y = element_line(colour="#D0D0D0"),
+      
+      #legend.position = "bottom",    To move the legend to bottom. I dont like it
+      
+    )
+}
