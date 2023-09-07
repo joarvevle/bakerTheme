41ee2bf699b323b261845b04ce40876334be010f
joarvevle <143795683+joarvevle@users.noreply.github.com>
2023-09-07 15:24:51 +0200

    Update mainFunction.R

--- R/mainFunction.R
+++ R/mainFunction.R
@@ -7,9 +7,9 @@ baker_fill <- function(values = baker){
 }
 
 theme_baker_dark <- function(legend=0, palette=baker_light){
-  list(theme_bakerX_base_dark(legend=legend), baker_colour(values = palette), baker_fill(values = palette))
+  list(theme_baker_base_dark(legend=legend), baker_colour(values = palette), baker_fill(values = palette))
 }
 
 theme_baker <- function(legend=0, palette=baker){
-  list(theme_bakerX_base(legend=legend), baker_colour(values = palette), baker_fill(values = palette))
+  list(theme_baker_base(legend=legend), baker_colour(values = palette), baker_fill(values = palette))
 }
